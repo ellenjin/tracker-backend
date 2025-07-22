cb2b56263ee4a3e75925bb8b49f6bfde92b1a32b
Solhee <ellen.solhee.jin@gmail.com>
2025-07-22 11:51:20 -0700

    Set up Gradle build system for Spring Boot; configured Gradle Java toolchain to require Java 17

---
+++ src/main/java/com/adadev/tracker_backend/TrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adadev.tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adadev/tracker_backend/TrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adadev.tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
