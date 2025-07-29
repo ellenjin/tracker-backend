f325c3918c08b854d6a6b05af2703209df65923e
Solhee <ellen.solhee.jin@gmail.com>
2025-07-29 09:49:13 -0700

    Adjusted user-route to search for one user by either userId or username in the same route.

--- src/main/java/com/adadev/tracker_backend/repository/UserRepository.java
+++ src/main/java/com/adadev/tracker_backend/repository/UserRepository.java
@@ -3,10 +3,11 @@ package com.adadev.tracker_backend.repository;
 import com.adadev.tracker_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     // You can add custom query methods here, like:
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username); // Optional is good for handling missing users
     // but there are also a lot of query methods that are already written in JpaRepository!
 }
